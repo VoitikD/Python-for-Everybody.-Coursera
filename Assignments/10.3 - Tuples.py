63187a3f6cded3ae5b82bc4e3ca3509a51c06c31
Dmitriy Voitik <voitikd@gmail.com>
2017-05-12 22:21:40 +0300

    Update 10.3 - Tuples.py

--- Assignments/10.3 - Tuples.py
+++ Assignments/10.3 - Tuples.py
@@ -1,7 +1,14 @@
+# Exercise 10.3 Write a program that reads a file and prints the letters in decreasing
+# order of frequency. Your program should convert all the input to lower case and
+# only count the letters a-z. Your program should not count spaces, digits, punctuation,
+# or anything other than the letters a-z. Find text samples from several different
+# languages and see how letter frequency varies between languages. Compare your
+# results with the tables at wikipedia.org/wiki/Letter_frequencies'
+
+
 name = raw_input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
-
 
 d = dict()
 for line in handle:
